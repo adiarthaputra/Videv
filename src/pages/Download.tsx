7e1eed64f03ee3085f97c5ee1bde132705a04e1e
adiarthaputra <124423360+adiarthaputra@users.noreply.github.com>
2025-12-16 00:00:01 +0800

    Update Download.tsx

--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -6,7 +6,6 @@ export function Download() {
 
   const randomUrls = [
     'https://otieu.com/4/10069259',
-    'https://viikqoye.com/dc/?blockID=399448',
     'https://viidedss.com/dc/?blockID=388556'
     'https://viiukuhe.com/dc/?blockID=410871'
   ];
